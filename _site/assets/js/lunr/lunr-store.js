d1367de444d64146f173482b4d28e05bd030754d
Mercedes Lovo <idalialovo7@gmail.com>
2024-06-21 14:29:43 +0200

    updated posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,10 @@ var store = [{
         "title": "Julia",
         "excerpt":"What happens if I take a function and evaluate it at a value, then use the result to evaluate the same function again, and then with this new result evaluate the same function again, and so on indefinitely. Let’s say I store these results in a set. Depending on the...","categories": [],
         "tags": [],
-        "url": "/julia/",
-        "teaser": null
-      },{
+        "url": "http://localhost:4000/julia/",
+        "teaser":null},{
         "title": "Mandelbrot",
         "excerpt":"If the Julia set is a way to graphically represent the behavior of a function when evaluated recursively given a seed or initial value, remember that it involves assigning a color in the complex number plane to each initial value depending on whether the function converges or diverges, and in...","categories": [],
         "tags": [],
-        "url": "/mandelbrot/",
-        "teaser": null
-      }]
+        "url": "http://localhost:4000/mandelbrot/",
+        "teaser":null}]
